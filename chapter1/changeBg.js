7570dc2d70f98534708af2d623363729745b6809
yeonjiheum <wlgma224@gmail.com>
2025-04-11 23:28:47 +0900

    Update changeBg.js

--- chapter1/changeBg.js
+++ chapter1/changeBg.js
@@ -2,7 +2,7 @@
 function random(number){
       return Math.floor(Math.random() * number);
     }
-    function bgChange(){
+function bgChange(){
       const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
       document.body.style.backgroundColor = rndCol;
     }
